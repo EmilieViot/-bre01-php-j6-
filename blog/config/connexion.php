1ce507cf9d528c13c661ab1c15575eeb8b641fc0
EmilieViot <e.larvol@outlook.fr>
2024-01-22 09:07:28 +0000

    adding all exercise

---
+++ blog/config/connexion.php
@@ -0,0 +1,15 @@
+<?php
+$host = "db.3wa.io";
+$port = "3306";
+$dbname = "emilieviot_posts";
+$connexionString = "mysql:host=$host;port=$port;dbname=$dbname";
+
+$user = "emilieviot";
+$password = "5a1798f311dc7281a54638f13a8bcb31";
+
+$db = new PDO(
+    $connexionString,
+    $user,
+    $password
+);
+
